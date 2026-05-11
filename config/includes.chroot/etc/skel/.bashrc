f03ad9404ce852486abfb0b4d9cd24401b4b0dbb
gabrielepccomputer-hub <gabrielepccomputer@gmail.com>
2026-05-11 18:37:01 +0000

    🚀 GAB OS: Dash animata macOS style + Effetti Blur e Trasparenza

--- config/includes.chroot/etc/skel/.bashrc
+++
@@ -1,5 +0,0 @@
-# GAB OS Bash Prompt - Modern Style
-export PS1="\[\e[35m\]GAB-OS\[\e[m\] \[\e[36m\]\w\[\e[m\] \[\e[32m\]🚀\[\e[m\] "
-alias ll='ls -la --color=auto'
-alias upgrade='sudo apt update && sudo apt upgrade'
-neofetch
--- config/includes.chroot/usr/share/plasma/shells/org.kde.plasma.desktop/contents/updates/gabos_galactic_dock.js
+++ config/includes.chroot/usr/share/plasma/shells/org.kde.plasma.desktop/contents/updates/gabos_galactic_dock.js
@@ -1,25 +1,27 @@
 var allPanels = panels();
 for (var i in allPanels) { allPanels[i].remove(); }
 
-// Barra Superiore (Sottile, Mac-style)
+// DASH BASSA: Fluttuante, Super Animata, Centrata
+var dock = new Panel();
+dock.location = "bottom";
+dock.height = 76; // Bella cicciotta per le animazioni
+dock.alignment = "center";
+dock.lengthMode = "fit"; 
+dock.floating = true; // Staccata dal bordo
+
+var taskManager = dock.addWidget("org.kde.plasma.icontasks");
+taskManager.currentConfigGroup = ["General"];
+// Aggiungiamo le app: Terminale, Browser, Impostazioni, Esplora File
+taskManager.writeConfig("launchers", "applications:org.kde.konsole.desktop,applications:firefox-esr.desktop,applications:systemsettings.desktop,applications:org.kde.dolphin.desktop");
+taskManager.writeConfig("showOnlyCurrentScreen", true);
+taskManager.writeConfig("wheelEnabled", true);
+
+// BARRA ALTO: Pulita e Galattica
 var topBar = new Panel();
 topBar.location = "top";
-topBar.height = 26;
-topBar.backgroundHints = 2;
+topBar.height = 30;
+topBar.backgroundHints = 2; // Trasparente
 topBar.addWidget("org.kde.plasma.kickoff");
 topBar.addWidget("org.kde.plasma.panelspacer");
 topBar.addWidget("org.kde.plasma.systemtray");
 topBar.addWidget("org.kde.plasma.digitalclock");
-
-// DASH BASSA (Animata, Fluttuante, Icone Grandi)
-var dock = new Panel();
-dock.location = "bottom";
-dock.height = 72;
-dock.alignment = "center";
-dock.lengthMode = "fit"; 
-dock.floating = true;
-dock.backgroundHints = 2;
-
-var taskManager = dock.addWidget("org.kde.plasma.icontasks");
-taskManager.currentConfigGroup = ["General"];
-taskManager.writeConfig("launchers", "applications:org.kde.konsole.desktop,applications:firefox-esr.desktop,applications:systemsettings.desktop");
